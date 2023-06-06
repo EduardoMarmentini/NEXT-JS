b6b524ab92f5461786b328ef5698db09bee4ffd5
EduardoDevword <eduardomarmentini.dev@gmail.com>
2023-06-06 17:23:39 -0300

    Dando continuidade a aula 47 e 48 do exercicio da mega, organizado o e aplicando a logica do programa "falta ajustes"

--- components/LayoutMega.jsx
+++ components/LayoutMega.jsx
@@ -1,33 +1,28 @@
+import { useState } from "react"
+import { GerarMega } from "../data/megasena"
+
 export default function LayoutMega(){
-    
+    const [numeros, setNumeros] = useState(GerarMega())
+
+    function rendLista(){
+       return numeros.map(valor => <div className="resultado" key = {valor}> {valor}</div>)
+    }
     
     return(
-        
-        <div>
-            <h1 className="tituloMega" style={{
-                textAlign :"center",
-                backgroundColor : "#969696"
-            }}>
+        <div > 
+            <h1 className="tituloMega">
                 MEGASENA NEXT
             </h1>
 
             <div className="corpoMega"> 
 
                 <div className="corpoResultados" >
-
-                    <div className="resultado">
-                        
-                    </div>
-
+                    {rendLista()}
                 </div>
 
             </div>
 
             <br/>
-
-            <div>
-                <button>Gerar</button>
-            </div>
         </div>
     )
 }
---
+++ data/megasena.js
@@ -0,0 +1,9 @@
+export function GerarMega(){
+    const lista = []
+
+    for (var i = 0 ; i < 6 ; i++){
+        lista.push(Math.floor(Math.random() * 99 + 1))
+    }
+}
+
+
